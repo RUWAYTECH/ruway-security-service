68173f38e27a3822977718b93e09e5592d086661
leyter <leyterespinoza5@gmail.com>
2026-05-13 11:50:04 -0500

    fix: controllerFixForUpdate

--- src/SecurityMicroservice.Api/Controllers/UsersController.cs
+++ src/SecurityMicroservice.Api/Controllers/UsersController.cs
@@ -52,7 +52,7 @@ public class UsersController : ControllerBase
     [HttpPut("{id}")]
     public async Task<ActionResult<UserDto>> UpdateUser(Guid id, BaseUserRequestDto request)
     {
-        var result = await _userService.UpdatePartial(id, request);
+        var result = await _userService.UpdatePartial(id, request, null);
         if (result.IsValid)
         {
             if (result.Data == null)
